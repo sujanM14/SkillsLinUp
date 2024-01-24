89b5e213703a4544ac89e3e79c1f9df7c3c5587c
sujanM14 <143599577+sujanM14@users.noreply.github.com>
2024-01-24 22:32:05 +0530

    Add files via upload

---
+++ client/src/components/student/ClubAssesment/ClubAssesment.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Body from "./Body";
+import Header from "../Header";
+import Sidebar from "../Sidebar";
+import { useDispatch } from "react-redux";
+import { createClubAssessment } from "../../../redux/actions/studentActions";
+import { getclub } from "../../../redux/actions/studentActions";
+
+const ClubAssesment = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getclub());
+  }, [dispatch]);
+  return (
+    <div className="bg-[#d6d9e0] h-screen flex items-center justify-center">
+      <div className="flex flex-col  bg-[#f4f6fa] h-5/6 w-[95%] rounded-2xl shadow-2xl space-y-6 ">
+        <Header />
+        <div className="flex flex-[0.95]">
+          <Sidebar />
+          <Body />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClubAssesment;
